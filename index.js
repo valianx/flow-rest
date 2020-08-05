ab18d17ff20cc1b8b1ea55f0dc4fa43c3b9bdc70
Valian <mgutiers3012@gmail.com>
2020-08-04 20:34:00 -0400

    test en servidor

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ const axios = require("axios");
 require('dotenv').config()
 const qs = require('querystring')
 const compression = require('compression')
+const morgan = require('morgan')
 
 port = 3000
 app.set('port'.port)
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(compression());
+app.use(morgan('dev'))
 
 app.post("/create_payment", async (req, res) => {
 
-    let { amount, subject, email } = req.body
+    let {
+        amount,
+        subject,
+        email
+    } = req.body
+
 
     if (amount == null) return res.status(400).json("amount es requerido")
     if (subject == null) return res.status(400).json("subject es requerido")
     if (email == null) return res.status(400).json("email es requerido")
-
     let commerceOrder = Date.now()
     let timeout = 60 * 10
 
     let data = "amount" + amount + "apiKey" + process.env.apiKey + "commerceOrder" + commerceOrder + "currency" + process.env.currency + "email" + email + "subject" + subject + "timeout" + timeout + "urlConfirmation" + process.env.urlConfirmation + "urlReturn" + process.env.urlReturn;
-
 
     let sign = CryptoJS(data, process.env.secretKey);
 
@@ -41,11 +48,15 @@ app.post("/create_payment", async (req, res) => {
         's': sign.toString()
     }
 
-    axios.post(process.env.apiURL + '/payment/create', qs.stringify(body), { headers: { 'Content-type': 'application/x-www-form-urlencoded' } }).then(response => {
-
+    axios.post(process.env.apiURL + '/payment/create', qs.stringify(body), {
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+    }).then(response => {
         return res.status(200).json({
             url: `${response.data.url}?token=${response.data.token}`,
-            flowOrder: response.data.flowOrder
+            flowOrder: response.data.flowOrder,
+            orden: commerceOrder
         })
 
     }).catch(
@@ -56,9 +67,9 @@ app.post("/create_payment", async (req, res) => {
     )
 })
 
+
 app.post('/getStatus', async (req, res) => {
     let token = req.body.token
-
     let object = "apiKey=" + process.env.apiKey + "&token=" + token;
     let sign = CryptoJS(object, process.env.secretKey);
 
@@ -70,7 +81,11 @@ app.post('/getStatus', async (req, res) => {
 })
 
 app.post('/refund_create', async (req, res) => {
-    const { refundCommerceOrder, receiverEmail, amount } = req.body
+    const {
+        refundCommerceOrder,
+        receiverEmail,
+        amount
+    } = req.body
     let urlCallBack = 'https://0d1bd9e31952.ngrok.io/refund'
 
     let data = "amount" + amount + "apiKey" + process.env.apiKey + "receiverEmail" + receiverEmail + "refundCommerceOrder" + refundCommerceOrder + "urlCallBack" + urlCallBack;
@@ -82,11 +97,15 @@ app.post('/refund_create', async (req, res) => {
         'apiKey': process.env.apiKey,
         'receiverEmail': receiverEmail,
         'refundCommerceOrder': refundCommerceOrder,
-        'urlCallBack': urlCallBack,
+        'urlCallBack': process.env.urlRefund,
         's': sign.toString()
     }
 
-    await axios.post(process.env.apiURL + '/refund/create', qs.stringify(body), { headers: { 'Content-type': 'application/x-www-form-urlencoded' } }).then(response => {
+    await axios.post(process.env.apiURL + '/refund/create', qs.stringify(body), {
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+    }).then(response => {
         console.log(response.data);
         return res.status(200).json("ok")
     }).catch(error => res.status(400).json(error))
